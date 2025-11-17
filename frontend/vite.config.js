bd9c483f9cc1a822da5df79cfeeb5690de18fa6b
KK <k.kadatoka@gmail.com>
2025-11-17 22:48:29 +1100

    build: raise chunk warning threshold

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,7 +17,8 @@ export default defineConfig(({ mode }) => {
     },
     build: {
       outDir: "dist",
-      sourcemap: false
+      sourcemap: false,
+      chunkSizeWarningLimit: 1024
     }
   };
 });
